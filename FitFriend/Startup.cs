c737f4ca76ee4383517eb0f64cb4927927c2753c
Joey McCoy <joey_mccoy@live.com>
2016-04-11 20:25:20 -0400

    initial commit

---
+++ FitFriend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FitFriend.Startup))]
+namespace FitFriend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
