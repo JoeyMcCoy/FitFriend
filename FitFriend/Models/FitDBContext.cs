2b83c1be8dc25a300d318e9ac6d5b0ca88783e72
Joey McCoy <joey_mccoy@live.com>
2016-04-12 23:42:54 -0400

    added azure sql database connection + models for calories and exercises

---
+++ FitFriend/Models/FitDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FitFriend.Models
+{
+    public class FitDBContext : DbContext
+    {
+        public FitDBContext(): base("SQLAzureConnection")
+        {
+            Database.SetInitializer<FitDBContext>(new DropCreateDatabaseIfModelChanges<FitDBContext>());
+        }
+        public DbSet<Calories> Calories { get; set; }
+        public DbSet<Exercises> Exercises { get; set; }
+    }
+}
