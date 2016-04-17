efbe2d205a6f2784d1fe570a427d1314ee6093fc
toddlasley <todd.r.lasley@gmail.com>
2016-04-17 14:58:51 -0400

    added ID (for database, intended to be primary key), Name, Description, and Calories properties to Food class

--- FitFriend/Models/Food.cs
+++ FitFriend/Models/Food.cs
@@ -7,5 +7,10 @@ namespace FitFriend.Models
 {
     public class Food
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Calories { get; set; }
+
     }
 }
